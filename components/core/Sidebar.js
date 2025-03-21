2449cf3e274c730c713f7c142a7a48ec0eaf646d
sparsh-DBALounge <sparsh.kadian@dbalounge.com>
2025-03-21 14:49:43 +0530

    css changes

--- components/core/Sidebar.js
+++ components/core/Sidebar.js
@@ -3,9 +3,12 @@ import { icons } from "@/env/icons"
 export const SideBar = () => {
     return (
         <div className="sidebar">
-            <button className="sidebar-btn">Add User</button>
-            <button className="sidebar-btn">Upload File</button>
-            <button className="sidebar-btn">Download File</button>
+            <div className="function-btns">
+                <button className="sidebar-btn">Dashboard</button>
+                <button className="sidebar-btn">Add User</button>
+                <button className="sidebar-btn">Upload File</button>
+                <button className="sidebar-btn">Download File</button>
+            </div>
             <button className="logout-btn">
                 <img src={icons.LOGOUT} alt="logout" width={20} height={20} />
                 Logout
